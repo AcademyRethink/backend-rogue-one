28a019e196de2e30515fa0dc539ac2b67d508ec6
Marcos Paulo <marcos.pereira@rethink.dev>
2023-06-22 17:27:44 -0300

    feat: Inventory table migration created

---
+++ src/database/20230622193016_create_inventory_table.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('inventory', (table) => {
+    table.increments('id').primary();
+    table.string('cnpj').references('cnpj').inTable('report');
+    table.string('product_name');
+    table.integer('product_id').references('product_id').inTable('report');
+    table.integer('quantity').checkPositive();
+    table.string('category').references('category').inTable('report');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('inventory');
+}
