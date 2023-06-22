651ae5a02f2420745aba60dae51380a1557cb26d
Marcos Paulo <marcos.pereira@rethink.dev>
2023-06-22 17:54:34 -0300

    fix: Change in reference of Invetory migration

--- src/database/20230622193016_create_inventory_table.ts
+++ src/database/20230622193016_create_inventory_table.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('inventory', (table) => {
     table.increments('id').primary();
-    table.string('cnpj').references('cnpj').inTable('report');
+    table.string('cnpj').references('cnpj').inTable('pharmacy');
     table.string('product_name');
     table.integer('product_id').references('product_id').inTable('report');
     table.integer('quantity').checkPositive();
