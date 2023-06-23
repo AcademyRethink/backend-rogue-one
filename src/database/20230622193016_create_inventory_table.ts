970e5915e5acf8a3411b3ad85784f244ba5c52d1
Marcos Paulo <marcos.pereira@rethink.dev>
2023-06-23 13:31:10 -0300

    fix: not nullable clauses added

--- src/database/20230622193016_create_inventory_table.ts
+++ src/database/20230622193016_create_inventory_table.ts
@@ -4,9 +4,13 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('inventory', (table) => {
     table.increments('id').primary();
     table.string('cnpj').references('cnpj').inTable('pharmacy');
-    table.string('product_name');
-    table.integer('product_id').references('product_id').inTable('report');
-    table.integer('quantity').checkPositive();
+    table.string('product_name').notNullable();
+    table
+      .integer('product_id')
+      .references('product_id')
+      .inTable('report')
+      .notNullable();
+    table.integer('quantity').checkPositive().notNullable();
     table.string('category').references('category').inTable('report');
   });
 }
