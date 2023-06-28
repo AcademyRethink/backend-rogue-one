7ef06785ed88be8c4ef23a22788783ed34732c5d
Matheus Dias <matheus.dias@rethink.dev>
2023-06-28 16:37:44 -0300

    refactor(report): moving report types to types folder

--- src/seeds/report_seeder.ts
+++ src/seeds/report_seeder.ts
@@ -1,34 +1,8 @@
 import { knex, Knex } from 'knex';
 import config from '../../knexfile';
+import { Product, FormatedProduct, ReportData } from '../types/report';
 
 const knexInstance = knex(config);
-
-type Product = {
-  row_id: number;
-  category: string;
-  ean: string;
-  molecule: string;
-  laboratory: string;
-  product_name: string;
-  sale_competitors_month: string;
-  sale_pharmacy_month: string;
-  month_year: string;
-};
-
-type FormatedProduct = {
-  cnpj: string;
-  category: string;
-  ean: string;
-  molecule: string;
-  laboratory: string;
-  product_name: string;
-  sale_competitors_month: number;
-  sale_pharmacy_month: number;
-  month_year: Date;
-};
-type ReportData = {
-  data: Product[];
-};
 
 const formatNumber = (text: string) => {
   return Math.round(
