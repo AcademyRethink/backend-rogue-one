f15303018b94202af2aef64ba8086009d6cd247a
luan <luan.barreiros@rethink.dev>
2023-07-04 15:20:44 -0300

    fix: spyOn was not necessary

--- src/services/tests/category.test.ts
+++ src/services/tests/category.test.ts
@@ -5,18 +5,15 @@ import categoryService from '../categoryService';
 describe('category test', () => {
   it('should return an object array', async () => {
     const result = await categoryService.getAll();
-    jest.spyOn(categoryRepository, 'getCategories');
     expect(typeof result).toBe('object');
   });
 
   it('should return a not null array', async () => {
     const result = await categoryService.getAll();
-    jest.spyOn(categoryRepository, 'getCategories');
     expect(result.length).not.toBeLessThanOrEqual(0);
   });
   it('should return an error if array is undefined', async () => {
     const result = await categoryService.getAll();
-    jest.spyOn(categoryRepository, 'getCategories');
     expect(typeof result.length).not.toBe('undefined');
   });
 });
