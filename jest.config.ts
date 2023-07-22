b54e50736996035aeec2a99169e70ec24bb7e129
Vinícius Delmo <vinicius.delmo@rethink.dev>
2023-07-22 16:32:10 -0300

    fix: eslint with jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,4 @@
-/* 
- * For a detailed explanation regarding each configuration property and type check, visit:
- 
-
-export default {
-  clearMocks: true,
-
-  collectCoverage: true,
-
-  coverageDirectory: 'coverage',
-
-  preset: 'ts-jest'
-}; */
- 
-
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   clearMocks: true,
 
