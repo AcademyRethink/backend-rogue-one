699f0b0c37763f6eaba79017e8527dce023affa6
Vinícius Delmo <vinicius.delmo@rethink.dev>
2023-07-22 15:17:14 -0300

    feat: functions websocket notifications

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application  } from 'express';
+/* import express, { Application  } from 'express';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
 import authRoutes from './routes/authRoute';
@@ -43,4 +43,54 @@ io.on('connection', (socket: Socket) => {
 
 httpServer.listen(port, () => {
   console.log(`Server listening on port ${port}. http://localhost:${port}`);
+}); */
+
+
+ import express, { Application } from 'express';
+import cors from "cors"
+import authRoutes from './routes/authRoute';
+import { dashboardRoute } from './routes/dashboardRoute';
+import { auth } from './middlewares/auth';
+import * as dotenv from 'dotenv';
+import { createServer } from 'http'; // Importe createServer do http
+import { Server, Socket } from 'socket.io';
+import notificationsService from './services/notificationsService';
+import notificationRoute from './routes/notificationRoute'; 
+
+dotenv.config();
+
+// Crie uma instância do aplicativo Express
+const app: Application = express();
+app.use(cors({ origin: '*' }));
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors())
+app.use('/auth', authRoutes);
+app.use('/', notificationRoute);
+
+ app.use(auth); 
+app.use('/dashboard', dashboardRoute);
+
+const httpServer = createServer(app); // Crie o servidor HTTP
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
 });
+
+io.on('connection', (socket: Socket) => {
+  console.log('Cliente conectado:', socket.id);
+
+  notificationsService.startProductQuantityCheck(socket);
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+httpServer.listen(port, () =>
+  console.log(`Server listening on port ${port}. http://localhost:${port}`)
+);
